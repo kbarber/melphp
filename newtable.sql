36bcadbd43d4ba1dd1ac179c3ee48cb0e53b020b
Ken Barber <ken@bob.sh>
2019-10-07 01:16:47 +0100

    Fixes

--- newtable.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS `rock_paper_scissors`;
-CREATE TABLE `rock_paper_scissors` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) NOT NULL,
-  `win` enum('yes','no','tie') NOT NULL,
-  PRIMARY KEY (`id`)
-);
